70dc9de1813767dfc877aaabad1360f40256cd11
yuxling <yuxling@cisco.com>
2020-11-18 10:38:44 +0800

    build pcre use visual studio

---
+++ glib20/gio/gio-marshal.h
@@ -0,0 +1,200 @@
+
+#ifndef ___gio_marshal_MARSHAL_H__
+#define ___gio_marshal_MARSHAL_H__
+
+#include	<glib-object.h>
+
+G_BEGIN_DECLS
+
+/* VOID:STRING,STRING,STRING,FLAGS (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:1) */
+extern void _gio_marshal_VOID__STRING_STRING_STRING_FLAGS (GClosure     *closure,
+                                                           GValue       *return_value,
+                                                           guint         n_param_values,
+                                                           const GValue *param_values,
+                                                           gpointer      invocation_hint,
+                                                           gpointer      marshal_data);
+
+/* VOID:STRING,BOXED (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:2) */
+extern void _gio_marshal_VOID__STRING_BOXED (GClosure     *closure,
+                                             GValue       *return_value,
+                                             guint         n_param_values,
+                                             const GValue *param_values,
+                                             gpointer      invocation_hint,
+                                             gpointer      marshal_data);
+
+/* VOID:STRING,VARIANT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:3) */
+extern void _gio_marshal_VOID__STRING_VARIANT (GClosure     *closure,
+                                               GValue       *return_value,
+                                               guint         n_param_values,
+                                               const GValue *param_values,
+                                               gpointer      invocation_hint,
+                                               gpointer      marshal_data);
+
+/* VOID:BOOLEAN,POINTER (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:4) */
+extern void _gio_marshal_VOID__BOOLEAN_POINTER (GClosure     *closure,
+                                                GValue       *return_value,
+                                                guint         n_param_values,
+                                                const GValue *param_values,
+                                                gpointer      invocation_hint,
+                                                gpointer      marshal_data);
+
+/* VOID:OBJECT,OBJECT,ENUM (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:5) */
+extern void _gio_marshal_VOID__OBJECT_OBJECT_ENUM (GClosure     *closure,
+                                                   GValue       *return_value,
+                                                   guint         n_param_values,
+                                                   const GValue *param_values,
+                                                   gpointer      invocation_hint,
+                                                   gpointer      marshal_data);
+
+/* BOOLEAN:OBJECT,OBJECT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:6) */
+extern void _gio_marshal_BOOLEAN__OBJECT_OBJECT (GClosure     *closure,
+                                                 GValue       *return_value,
+                                                 guint         n_param_values,
+                                                 const GValue *param_values,
+                                                 gpointer      invocation_hint,
+                                                 gpointer      marshal_data);
+
+/* VOID:STRING,BOXED,BOXED (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:7) */
+extern void _gio_marshal_VOID__STRING_BOXED_BOXED (GClosure     *closure,
+                                                   GValue       *return_value,
+                                                   guint         n_param_values,
+                                                   const GValue *param_values,
+                                                   gpointer      invocation_hint,
+                                                   gpointer      marshal_data);
+
+/* BOOL:POINTER,INT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:8) */
+extern void _gio_marshal_BOOLEAN__POINTER_INT (GClosure     *closure,
+                                               GValue       *return_value,
+                                               guint         n_param_values,
+                                               const GValue *param_values,
+                                               gpointer      invocation_hint,
+                                               gpointer      marshal_data);
+#define _gio_marshal_BOOL__POINTER_INT	_gio_marshal_BOOLEAN__POINTER_INT
+
+/* BOOL:UINT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:9) */
+extern void _gio_marshal_BOOLEAN__UINT (GClosure     *closure,
+                                        GValue       *return_value,
+                                        guint         n_param_values,
+                                        const GValue *param_values,
+                                        gpointer      invocation_hint,
+                                        gpointer      marshal_data);
+#define _gio_marshal_BOOL__UINT	_gio_marshal_BOOLEAN__UINT
+
+/* BOOL:VARIANT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:10) */
+extern void _gio_marshal_BOOLEAN__VARIANT (GClosure     *closure,
+                                           GValue       *return_value,
+                                           guint         n_param_values,
+                                           const GValue *param_values,
+                                           gpointer      invocation_hint,
+                                           gpointer      marshal_data);
+#define _gio_marshal_BOOL__VARIANT	_gio_marshal_BOOLEAN__VARIANT
+
+/* BOOL:VOID (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:11) */
+extern void _gio_marshal_BOOLEAN__VOID (GClosure     *closure,
+                                        GValue       *return_value,
+                                        guint         n_param_values,
+                                        const GValue *param_values,
+                                        gpointer      invocation_hint,
+                                        gpointer      marshal_data);
+#define _gio_marshal_BOOL__VOID	_gio_marshal_BOOLEAN__VOID
+
+/* VOID:STRING,STRING,BOXED (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:12) */
+extern void _gio_marshal_VOID__STRING_STRING_BOXED (GClosure     *closure,
+                                                    GValue       *return_value,
+                                                    guint         n_param_values,
+                                                    const GValue *param_values,
+                                                    gpointer      invocation_hint,
+                                                    gpointer      marshal_data);
+
+/* VOID:BOOL,BOXED (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:13) */
+extern void _gio_marshal_VOID__BOOLEAN_BOXED (GClosure     *closure,
+                                              GValue       *return_value,
+                                              guint         n_param_values,
+                                              const GValue *param_values,
+                                              gpointer      invocation_hint,
+                                              gpointer      marshal_data);
+#define _gio_marshal_VOID__BOOL_BOXED	_gio_marshal_VOID__BOOLEAN_BOXED
+
+/* VOID:VARIANT,VARIANT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:14) */
+extern void _gio_marshal_VOID__VARIANT_VARIANT (GClosure     *closure,
+                                                GValue       *return_value,
+                                                guint         n_param_values,
+                                                const GValue *param_values,
+                                                gpointer      invocation_hint,
+                                                gpointer      marshal_data);
+
+/* VOID:INT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:15) */
+#define _gio_marshal_VOID__INT	g_cclosure_marshal_VOID__INT
+
+/* VOID:STRING,INT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:16) */
+extern void _gio_marshal_VOID__STRING_INT (GClosure     *closure,
+                                           GValue       *return_value,
+                                           guint         n_param_values,
+                                           const GValue *param_values,
+                                           gpointer      invocation_hint,
+                                           gpointer      marshal_data);
+
+/* VOID:STRING,UINT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:17) */
+extern void _gio_marshal_VOID__STRING_UINT (GClosure     *closure,
+                                            GValue       *return_value,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint,
+                                            gpointer      marshal_data);
+
+/* VOID:BOXED,BOXED (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:18) */
+extern void _gio_marshal_VOID__BOXED_BOXED (GClosure     *closure,
+                                            GValue       *return_value,
+                                            guint         n_param_values,
+                                            const GValue *param_values,
+                                            gpointer      invocation_hint,
+                                            gpointer      marshal_data);
+
+/* VOID:VARIANT,BOXED (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:19) */
+extern void _gio_marshal_VOID__VARIANT_BOXED (GClosure     *closure,
+                                              GValue       *return_value,
+                                              guint         n_param_values,
+                                              const GValue *param_values,
+                                              gpointer      invocation_hint,
+                                              gpointer      marshal_data);
+
+/* VOID:STRING,STRING,VARIANT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:20) */
+extern void _gio_marshal_VOID__STRING_STRING_VARIANT (GClosure     *closure,
+                                                      GValue       *return_value,
+                                                      guint         n_param_values,
+                                                      const GValue *param_values,
+                                                      gpointer      invocation_hint,
+                                                      gpointer      marshal_data);
+
+/* VOID:STRING (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:21) */
+#define _gio_marshal_VOID__STRING	g_cclosure_marshal_VOID__STRING
+
+/* VOID:STRING,STRING (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:22) */
+extern void _gio_marshal_VOID__STRING_STRING (GClosure     *closure,
+                                              GValue       *return_value,
+                                              guint         n_param_values,
+                                              const GValue *param_values,
+                                              gpointer      invocation_hint,
+                                              gpointer      marshal_data);
+
+/* VOID:STRING,BOOLEAN (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:23) */
+extern void _gio_marshal_VOID__STRING_BOOLEAN (GClosure     *closure,
+                                               GValue       *return_value,
+                                               guint         n_param_values,
+                                               const GValue *param_values,
+                                               gpointer      invocation_hint,
+                                               gpointer      marshal_data);
+
+/* BOOL:OBJECT (D:\work\svn\cpve_trunk\external\glib\gio\gio-marshal.list:24) */
+extern void _gio_marshal_BOOLEAN__OBJECT (GClosure     *closure,
+                                          GValue       *return_value,
+                                          guint         n_param_values,
+                                          const GValue *param_values,
+                                          gpointer      invocation_hint,
+                                          gpointer      marshal_data);
+#define _gio_marshal_BOOL__OBJECT	_gio_marshal_BOOLEAN__OBJECT
+
+G_END_DECLS
+
+#endif /* ___gio_marshal_MARSHAL_H__ */
+
